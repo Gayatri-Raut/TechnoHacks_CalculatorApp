7b6cc2722598655f862cc80bee7e8111cfe3852e
Gayatri <gayatriraut703@gmail.com>
2023-08-21 14:24:26 +0530

    first commit

---
+++ CalculatorApp.java
@@ -0,0 +1,45 @@
+git import java.util.Scanner;
+
+public class CalculatorApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Simple Calculator App");
+        System.out.println("Available operations: +, -, *, /");
+        System.out.print("Enter first number: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Enter an operation: ");
+        char operation = scanner.next().charAt(0);
+
+        System.out.print("Enter second number: ");
+        double num2 = scanner.nextDouble();
+
+        double result = 0.0;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Invalid operation.");
+                return;
+        }
+
+        System.out.println("Result: " + result);
+    }
+}
